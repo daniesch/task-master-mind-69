33f897d929ad09377c1d1e5dbb78be0211e35c7a
Dansch <spam@schuler.ooo>
2026-06-04 09:23:37 +0200

    first commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.96e76ba2ae74432fa5a196204669ac5d',
+  appId: 'tast.schuler.ooo.app',
   appName: 'Tasks',
   webDir: 'dist',
   ios: {
